599d5a807a059d5d572b8e1907c46161f56899c4
gal <galsinay128@gmail.com>
2018-06-30 10:59:47 +0300

    setting weather

--- js/services/weather.service.js
+++ js/services/weather.service.js
@@ -3,14 +3,14 @@ const WEATHER_API_KEY = '58133143c32b3b8e0cc20f0bd16e69b4';
 function getWeatherFromApi(pos) {
     console.log('pos',pos)
     return axios.get(
-        `http://api.openweathermap.org/data/2.5/weather?lat=${pos.lat}&lon=${pos.lng}&APPID=${WEATHER_API_KEY}`);
+        `https://api.openweathermap.org/data/2.5/weather?lat=${pos.lat}&lon=${pos.lng}&APPID=${WEATHER_API_KEY}`);
 }
 
 function renderWeather(weather){
     var elWeather = document.querySelector('.curr-temp');
-    elWeather.innerText = weather.main.temp + ' ℃';
+    elWeather.innerText = (weather.main.temp -273.15).toFixed(2) + ' ℃';
     var elWeather = document.querySelector('.curr-temp-range');
-    elWeather.innerText = 'tempeture from ' + weather.main.temp_min + ' to ' + weather.main.temp_max;
+    elWeather.innerText = 'tempeture from ' + (weather.main.temp_min -273.15).toFixed(2) + ' ℃' + ' to ' + (weather.main.temp_max -273.15).toFixed(2) + ' ℃';
     elWeather = document.querySelector('.loc-name-weather');
     elWeather.innerText = weather.name;
     elWeather = document.querySelector('.weather-description');
